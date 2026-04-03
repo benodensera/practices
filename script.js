6bf45df9a5180e0572aade92acff8cd21b11048f
DevTech-Agency <devtechagency.ec@gmail.com>
2026-04-03 01:45:42 -0500

    *

--- script.js
+++ script.js
@@ -1,30 +1,4 @@
-/* const slides = document.querySelectorAll(".slide");
-const nextBtn = document.querySelector(".next");
-const prevBtn = document.querySelector(".prev");
-
-const slidesContainer = document.querySelector(".slides");
-let index = 0;
-
-function updateSlide() {
-  slidesContainer.style.transform = `translateX(-${index * 100}%)`;
-}
-
-function nextSlide() {
-  index = (index + 1) % slides.length;
-  updateSlide();
-}
-
-function prevSlide() {
-  index = (index - 1 + slides.length) % slides.length;
-  updateSlide();
-}
-
-nextBtn.addEventListener("click", nextSlide);
-prevBtn.addEventListener("click", prevSlide);
-
-// AUTO
-setInterval(nextSlide, 9000); */
-
+// hero slider
 
 const slides = document.querySelectorAll(".slide");
 let index = 0;
@@ -48,3 +22,37 @@ document.querySelector(".next").addEventListener("click", nextSlide);
 document.querySelector(".prev").addEventListener("click", prevSlide);
 
 setInterval(nextSlide, 9000);
+
+// mouse icon to next section
+
+document.querySelector(".mouse").addEventListener("click", () => {
+  window.scrollBy({
+    top: 600,
+    behavior: "smooth"
+  });
+});
+
+
+
+const filters = document.querySelectorAll(".filter");
+const items = document.querySelectorAll(".item");
+
+filters.forEach(btn => {
+  btn.addEventListener("click", () => {
+
+    // active button
+    filters.forEach(b => b.classList.remove("active"));
+    btn.classList.add("active");
+
+    const filter = btn.dataset.filter;
+
+    items.forEach(item => {
+      if (filter === "all" || item.classList.contains(filter)) {
+        item.style.display = "block";
+      } else {
+        item.style.display = "none";
+      }
+    });
+
+  });
+});
